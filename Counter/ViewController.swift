736de96e6840f920140efca42b4143a25bd5c4c8
nbastreetgame <129016494+nbastreetgame@users.noreply.github.com>
2023-10-17 20:34:46 +0700

    создание проекта

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Counter
+//  счетчик
 //
 //  Created by Apple on 17.10.2023.
 //
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    
+    
+    @IBAction func buttom(_ sender: UIButton) {
+        counter += 1
+        label.text = "\(counter)"
+        textView.text = textView.text + "\n \(Date()) значение изменено на +1"
+    }
+    
+   
+    @IBAction func minusButtom(_ sender: UIButton) {
+        if counter > 0 {counter -= 1
+            
+            label.text = "\(counter)"
+            textView.text = textView.text + "\n \(Date()) значение изменено на -1"
+            
+            
+        } else {
+            textView.text = textView.text + "\n \(Date()) попытка умешить значение ниже 0"
+        }
+    
+        
+    }
+    
+    @IBAction func resetButtom(_ sender: UIButton) { 
+        counter = 0
+        label.text = "\(counter)"
+        textView.text = textView.text + "\n \(Date()) значение сброшено"
+    }
+    
+    
+    
+    @IBOutlet var textView: UITextView!
+    
+    
+    var counter = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textView.text = "История изменений:"
+        label.text = "\(counter)"
+        
     }
-
-
 }
-
